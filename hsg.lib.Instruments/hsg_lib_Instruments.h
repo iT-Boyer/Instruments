9638ed492ca09285915a9824b3b2e5614ed8dc9a
huos3203 <724987481@qq.com>
2018-11-13 18:38:11 +0800

    Initial Commit

---
+++ hsg.lib.Instruments/hsg_lib_Instruments.h
@@ -0,0 +1,19 @@
+//
+//  hsg_lib_Instruments.h
+//  hsg.lib.Instruments
+//
+//  Created by admin on 2018/11/13.
+//  Copyright © 2018年 clcw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for hsg_lib_Instruments.
+FOUNDATION_EXPORT double hsg_lib_InstrumentsVersionNumber;
+
+//! Project version string for hsg_lib_Instruments.
+FOUNDATION_EXPORT const unsigned char hsg_lib_InstrumentsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <hsg_lib_Instruments/PublicHeader.h>
+
+
---
+++ hsg.lib.InstrumentsTests/hsg_lib_InstrumentsTests.swift
@@ -0,0 +1,34 @@
+//
+//  hsg_lib_InstrumentsTests.swift
+//  hsg.lib.InstrumentsTests
+//
+//  Created by admin on 2018/11/13.
+//  Copyright © 2018年 clcw. All rights reserved.
+//
+
+import XCTest
+@testable import hsg_lib_Instruments
+
+class hsg_lib_InstrumentsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
