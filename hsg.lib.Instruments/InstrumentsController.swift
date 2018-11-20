c51a0407c74d74111c4d21a69a8f995d4f243e34
huos3203 <724987481@qq.com>
2018-11-20 20:52:57 +0800

    模拟数据

--- hsg.lib.Instruments/InstrumentsController.swift
+++ hsg.lib.Instruments/InstrumentsController.swift
@@ -21,7 +21,7 @@ class InstrumentsController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         //模拟数据
-        InstallHTTPStubs.activateHttpStub()
+        InstallHTTPStubs().installJSONDataStub(plist: "")
         navigationItem.backBarButtonItem = UIBarButtonItem(title: "Search", style: .plain, target: nil, action: nil)
     }
     
